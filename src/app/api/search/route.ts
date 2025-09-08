40e4433a78b2dff370d5c2039c37687f3653a387
m.hesam moradian <m.hesam.moradian@gmail.com>
2025-09-08 09:07:13 +0330

    this is sick

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -2,6 +2,7 @@
 
 import prisma from "@/lib/client";
 import { NextResponse } from "next/server";
+export const dynamic = "force-dynamic";
 
 export async function GET(req: Request) {
   const { searchParams } = new URL(req.url);
