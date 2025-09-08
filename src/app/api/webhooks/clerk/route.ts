396947482af453b5fecfc9c7d3031a93cce13e33
m.hesam moradian <m.hesam.moradian@gmail.com>
2025-09-08 09:15:04 +0330

    oh my godddddd what is it

--- src/app/api/webhooks/clerk/route.ts
+++ src/app/api/webhooks/clerk/route.ts
@@ -2,6 +2,7 @@ import { Webhook } from "svix";
 import { headers } from "next/headers";
 import { WebhookEvent } from "@clerk/nextjs/server";
 import prisma from "@/lib/client";
+export const dynamic = "force-dynamic";
 
 export async function POST(req: Request) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the endpoint
