02628558dff211d53afb7c2e5fd398160276f2c8
m.hesam moradian <m.hesam.moradian@gmail.com>
2025-09-30 13:50:00 +0330

    change the login logic

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+
 import Navbar from "@/components/Navbar";
 import { ClerkProvider } from "@clerk/nextjs";
 
@@ -18,6 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
+      {/* sd */}
       <html lang="en">
         <body className={inter.className}>
           <div className="w-full bg-white px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
