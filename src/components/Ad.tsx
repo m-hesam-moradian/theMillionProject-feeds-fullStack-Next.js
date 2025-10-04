df2a9be12e7b9229ca33db1a5e0904c630591e76
m.hesam moradian <m.hesam.moradian@gmail.com>
2025-10-04 14:19:54 +0330

    clear

--- src/components/Ad.tsx
+++ src/components/Ad.tsx
@@ -3,12 +3,11 @@ import Image from "next/image";
 const Ad = ({ size }: { size: "sm" | "md" | "lg" }) => {
   return (
     <div className="relative p-4 bg-white rounded-lg shadow-md text-sm ">
-      {/* TOP */}
       <div className="flex items-center justify-between text-gray-500 font-medium">
         <span>Sponsored Ads</span>
         <Image src="/more.png" alt="" width={16} height={16} />
       </div>
-      {/* BOTTOM */}
+
       <div
         className={`flex flex-col mt-4 ${size === "sm" ? "gap-2" : "gap-4"}`}
       >
