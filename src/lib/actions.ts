8042c4cafaebf3349a3d8d1a37f3a818d2130191
m.hesam moradian <m.hesam.moradian@gmail.com>
2025-10-04 14:12:22 +0330

    clear

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -240,8 +240,6 @@ export const switchLike = async (postId: number) => {
 };
 
 export const addComment = async (postId: number, desc: string) => {
-  // const { userId } = auth();
-
   if (!userId) throw new Error("User is not authenticated!");
 
   try {
