7bf1b915bd5c1876c00df966e0a1fde462d6357b
m.hesam moradian <m.hesam.moradian@gmail.com>
2025-11-05 18:11:00 +0330

    the comment get section is done

--- src/components/feed/Comments.tsx
+++ src/components/feed/Comments.tsx
@@ -86,6 +86,7 @@ const Comments = ({ postInfo, userInfo }: { postInfo: any; userInfo: any }) => {
         </div>
       )}
 
+
       {optimisticComments.length > 0 && (
         <div className="m-2 p-2 shadow-inner rounded-lg bg-slate-100  ">
           {optimisticComments.map((comment, index) => (
